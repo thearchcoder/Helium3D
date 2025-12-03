9b90a28505e4c908b9828d27b395a3a678f17ea6
thearchcoder <ahmadchawla1432@gmail.com>
2025-12-03 20:23:40 +0500

    Remove unused popup function

--- scenes/global.gd
+++ scenes/global.gd
@@ -13,25 +13,3 @@ func path(linux_path: String) -> String:
 	if OS.get_name() == "Windows":
 		return linux_path.replace('/', '\\')
 	return linux_path
-
-func show_yes_no_popup(title: String, message: String, parent_node: Node = null) -> bool:
-	var confirmation_dialog := AcceptDialog.new()
-	confirmation_dialog.title = title
-	confirmation_dialog.dialog_text = message
-	confirmation_dialog.add_cancel_button("No")
-	
-	if parent_node == null:
-		get_tree().current_scene.add_child(confirmation_dialog)
-	else:
-		parent_node.add_child(confirmation_dialog)
-	
-	confirmation_dialog.popup_centered()
-	
-	var result := false
-	confirmation_dialog.confirmed.connect(func() -> void: result = true)
-	confirmation_dialog.canceled.connect(func() -> void: result = false)
-	
-	await confirmation_dialog.close_requested
-	confirmation_dialog.queue_free()
-	
-	return result
